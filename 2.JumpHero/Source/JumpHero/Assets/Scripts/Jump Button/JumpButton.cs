b87f806cf59ea59544fa72f81ca90f4694a8c995
ngohongthai <ngohongthai.uet@gmail.com>
2016-04-18 00:15:45 +0700

    Finished
    Finish Jump hero tutorial

---
+++ 2.JumpHero/Source/JumpHero/Assets/Scripts/Jump Button/JumpButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class JumpButton : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+    public void OnPointerDown (PointerEventData data)
+    {
+        if (PlayerJumpScripts.instance != null)
+        {
+            PlayerJumpScripts.instance.SetPower(true);
+        }
+        
+    }
+
+    public void OnPointerUp (PointerEventData data)
+    {
+        if (PlayerJumpScripts.instance != null)
+        {
+            PlayerJumpScripts.instance.SetPower(false);
+        }
+    }
+
+	
+}
---
+++ 2.JumpHero/Source/JumpHero/Assets/Scripts/Main Menu Controller/MainMenuScript.cs
@@ -0,0 +1,1 @@
+﻿using UnityEngine;using System.Collections;using UnityEngine.SceneManagement;public class MainMenuScript : MonoBehaviour {    public void PlayGame ()    {        SceneManager.LoadScene("GamePlay");            }}
