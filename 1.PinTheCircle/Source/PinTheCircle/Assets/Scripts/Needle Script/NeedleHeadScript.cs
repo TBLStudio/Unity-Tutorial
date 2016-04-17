84c8594691110dafa634c63a86df0c7dcbbe6251
ngohongthai <ngohongthai.uet@gmail.com>
2016-04-17 11:38:56 +0700

    Finished.

---
+++ 1.PinTheCircle/Source/PinTheCircle/Assets/Scripts/Needle Script/NeedleHeadScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeedleHeadScript : MonoBehaviour {
+
+    void OnTriggerEnter2D (Collider2D target)
+    {
+        if (target.tag == "Needle Head")
+        {
+            print("Game Over");
+
+            Time.timeScale = 0f;
+        }
+    }
+}
